cae60e387839e0f200e55a06c9545e4c39d8e8ae
MrNexiol <mrnexiol@gmail.com>
2019-11-30 19:58:33 +0100

    Added basic comments to MealPlannerHelper

--- app/src/main/java/com/tkkd/mealplanner/MealPlannerHelper.java
+++ app/src/main/java/com/tkkd/mealplanner/MealPlannerHelper.java
@@ -7,24 +7,30 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class MealPlannerHelper extends SQLiteOpenHelper {
 
+    //Database related variables
     private static final String DB_NAME = "meal_planner";
     private static final int DB_VERSION = 1;
 
+    //Tables names
     private static final String AT_HOME = "AT_HOME";
     private static final String INGREDIENTS = "INGREDIENTS";
     static final String MEASURES = "MEASURE";
 
+    //Class constructor
     MealPlannerHelper(Context context){
         super(context,DB_NAME,null,DB_VERSION);
     }
 
+    //First database creation
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        //SQL command creating "MEASURE" table
         String create_tab_measures = "CREATE TABLE " + MEASURES + "(\n" +
                 "    _id integer NOT NULL CONSTRAINT measures_pk PRIMARY KEY AUTOINCREMENT,\n" +
                 "    measure_name text\n" +
                 ");";
 
+        //SQL command creating "INGREDIENTS" table
         String create_tab_ingredients = "CREATE TABLE " + INGREDIENTS + "(\n" +
                 "    _id integer NOT NULL CONSTRAINT ingredients_pk PRIMARY KEY AUTOINCREMENT,\n" +
                 "    name text,\n" +
@@ -33,6 +39,7 @@ public class MealPlannerHelper extends SQLiteOpenHelper {
                 "    REFERENCES measures (_id)\n" +
                 ");";
 
+        //SQL command creating "AT_HOME" table
         String create_tab_at_home = "CREATE TABLE " + AT_HOME + "(\n" +
                 "    _id integer NOT NULL CONSTRAINT at_home_pk PRIMARY KEY AUTOINCREMENT,\n" +
                 "    ingredients__id integer NOT NULL,\n" +
@@ -41,17 +48,22 @@ public class MealPlannerHelper extends SQLiteOpenHelper {
                 "    REFERENCES ingredients (_id)\n" +
                 ");";
 
+        //Actual creation of database
         sqLiteDatabase.execSQL(create_tab_measures);
         sqLiteDatabase.execSQL(create_tab_ingredients);
         sqLiteDatabase.execSQL(create_tab_at_home);
+
+        //Populate "Measure" table with data
         insertMeasure(sqLiteDatabase,"Test");
     }
 
+    //Update of existing databse
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
     }
 
+    //Method adding data to "Measure" table
     private void insertMeasure(SQLiteDatabase sqLiteDatabase, String mesName){
         ContentValues contentValues = new ContentValues();
         contentValues.put("measure_name",mesName);
